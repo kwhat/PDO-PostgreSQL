8138aba2309bd9693066d1ef7378073c2924283b
Alexander Barker <alex@1stleg.com>
2020-04-13 22:41:32 -0700

    Externalizing PostgreSQL syntax to its own library.

---
+++ src/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @license MIT
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace FaaPz\PDO\PostgreSQL;
+
+use FaaPz\PDO;
+
+class Database extends PDO\Database
+{
+    /**
+     * @param array<int|string, string> $columns
+     *
+     * @return Statement\Select
+     */
+    public function select(array $columns = ['*']): PDO\Statement\Select
+    {
+        return new Statement\Select($this, $columns);
+    }
+
+    /**
+     * @param array<string, mixed> $pairs
+     *
+     * @return Statement\Update
+     */
+    public function update(array $pairs = []): PDO\Statement\Update
+    {
+        return new Statement\Update($this, $pairs);
+    }
+
+    /**
+     * @param string|array<string, string> $table
+     *
+     * @return Statement\Delete
+     */
+    public function delete($table = null): PDO\Statement\Delete
+    {
+        return new Statement\Delete($this, $table);
+    }
+}
